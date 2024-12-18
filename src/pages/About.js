aa0be0761b5daa04b542a037268513634ed841c6
Xnihilo7 <jcifonie@liberty.edu>
2024-12-17 21:57:45 -0600

    Initial commit v2

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is where you can talk about yourself and your skills.</p>
+    </div>
+  );
+}
+
+export default About;
