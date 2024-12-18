f05a570a0896cd37c4bf0c3471ad6f23cebd7afc
Xnihilo7 <jcifonie@liberty.edu>
2024-12-17 23:20:14 -0600

    Updated React components and styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Header from './components/Header'; // Import Header component
 import './App.css';
 
 function App() {
@@ -21,6 +22,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+
+        <Hero /> {/* Use Header component */}
       </div>
     </Router>
   );
