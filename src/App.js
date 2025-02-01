080f8b9c2b7e1ffcdd91519fb1392b3e9ba51e21
Xnihilo7 <jcifonie@liberty.edu>
2025-01-31 23:48:57 -0600

    New changes 2025

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Home />
         <Footer />
       </div>
     </Router>
